282833dd9e1b5e8bd0bef8c1141f4263b3f6ac26
Kobe Swier <kobe.swier@southeasttech.edu>
2024-10-06 22:56:38 -0500

    12.

--- Ch04Ex1MovieList/Startup.cs
+++ Ch04Ex1MovieList/Startup.cs
@@ -1,6 +1,8 @@
+using Ch04Ex1MovieList.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,7 @@ namespace Ch04Ex1MovieList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<MovieContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
